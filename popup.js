214ada9530c9e1132be9e3d1b6ada01de6e6f054
John Mertens <john.mertens@gmail.com>
2013-02-03 20:32:52 -0800

    Minor debugging lines

--- popup.js
+++ popup.js
@@ -6,6 +6,11 @@ $(function() {
         api_key = $('#api_key').val() || 'abc123',
         article_id = $('#article_id').val() || '1';
     
+    console.log(selector);
+    console.log(api_key);
+    console.log(article_id);
+    
+    console.log()
     return false;
   });
 });
